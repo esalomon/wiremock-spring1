fb3672d706bb09c35378f566136c4c4c256d1091
Efrain Salomon <efrain_salomon@yahoo.com.mx>
2022-04-23 17:38:09 -0400

    Repository initial version.

---
+++ src/main/java/com/example/WireMockSpringApp.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WireMockSpringApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WireMockSpringApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/WireMockSpringAppTests.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class WireMockSpringAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
